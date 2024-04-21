0f49642a7c1c6cdf2ff7fda89dcf3dc2420b9aff
cveniegas <143545348+cveniegas@users.noreply.github.com>
2024-04-21 02:27:02 -0700

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,43 @@
+const { request } = require("express");
+
+const mysql = require("mysql");
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
 
 exports.register = (req,res) =>{
     console.log(req.body);
-    res.send("Form submitted");
+    // const name = request.body.name;
+    // const email = request.body.email;
+    // const password = requues.bod.password;
+    // const passwordConfirm = requues.bod.passwordConfirm;
+
+    const { name, email, password, passwordConfirm} = req.body;
+
+    db.query('SELECT email FROM users Where email = ?', [email],async(error, results) =>{
+        if (error){
+            console.log(error);
+        }
+        if (results.length > 0 ){
+            return results.render('register', {
+                message: 'That email is already in use'
+            })
+        } else if(password !== passwordConfirm){
+            return res.render ('register', {
+                message: 'Passwords do not match'
+            });
+        }
+        let hashedPassword = await bcrypt.hash(password, 8);
+        console.log(hashedPassword);
+        
+        res.send("testing")
+    });
+
+
 }
