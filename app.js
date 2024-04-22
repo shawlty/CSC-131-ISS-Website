d1e527f26b568548245c7995e4f465c88f305195
Sam Hammer <sam.hammer907@gmail.com>
2024-04-22 14:57:09 -0700

    forum 5

--- app.js
+++ app.js
@@ -1,45 +1,41 @@
 const express = require("express");
 const path = require('path');
-const app = express();
 const mysql = require("mysql");
 const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const Handlebars = require('handlebars');
-dotenv.config({ path: './.env'})
+
+dotenv.config({ path: './.env'});
+
+const app = express();
 
 const db = mysql.createConnection({
-    database: process.env.DATABASE,
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
 
-
-const publicDirectory = path.join(__dirname, './public')
-
+const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
-// Parse URL-encoded bodies
-app.use(express.urlencoded({ extended:false}));
-// Paerse Json bodies
+// parse URL-encoded bodies (sent by HTML form)
+app.use(express.urlencoded({ extended: false }));
+// parse JSON bodies
 app.use(express.json());
-app.use(cookieParser());
 
 app.set('view engine', 'hbs');
 
-
-db.connect( (error) =>{
-    if(error){
-        console.log(error)
-    } else {
-        console.log("MYSQL Connected... ")
-    }
+db.connect( (error) => {
+if(error) {
+    console.log(error)
+} else {
+    console.log("MYSQL Connected...")
+}
 })
 
-//Define Routes for pages
+//Define routes
 app.use('/', require('./routes/pages'));
-app.use('/auth',require('./routes/auth'));
+app.use('/auth', require('./routes/auth'));
 
 app.listen(5001, () => {
-    console.log ("Server started on port 5001");
+    console.log("Server started on Port 5001")
 })
